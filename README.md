fab3e60c74c68bdf91091c038ecf9eba2ac8ad9b
Tieg Zaharia <tieg@tidelift.com>
2020-03-28 18:34:31 -0400

    Add a -dotOnly flag to only output dot instead of image.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # pkgviz-go
 
-Generate a vizualization of a Go package's types.
-
 [![Go Report Card](https://goreportcard.com/badge/github.com/tiegz/pkgviz-go)](https://goreportcard.com/report/github.com/tiegz/pkgviz-go)
 [![GoDoc](https://godoc.org/github.comtiegz/pkgviz-go?status.svg)](https://godoc.org/github.com/tiegz/pkgviz-go)
+
+Generate a vizualization of a Go package's types.
 
 ## How does it work
 
--- cmd/pkgviz/main.go
+++ cmd/pkgviz/main.go
@@ -12,8 +12,8 @@ import (
 )
 
 func main() {
+	dotOnly := flag.Bool("dotOnly", false, "Only output the dot file text instead of writing to an image.")
 	flag.Parse()
-
 	args := flag.Args()
 
 	if len(args) == 0 {
@@ -22,20 +22,25 @@ func main() {
 	}
 	dotFile := pkgviz.WriteGraph(args[0])
 
-	imageFilename := "out.png"
-	cmd := exec.Command("dot", "-Tpng", "-o", imageFilename)
-	stdin, _ := cmd.StdinPipe()
-	go func() {
-		defer stdin.Close()
-		io.WriteString(stdin, dotFile)
-	}()
+	if (*dotOnly) == true {
+		fmt.Println(dotFile)
+	} else {
+		imageFilename := "out.png"
+		cmd := exec.Command("dot", "-Tpng", "-o", imageFilename)
+		stdin, _ := cmd.StdinPipe()
+		go func() {
+			defer stdin.Close()
+			io.WriteString(stdin, dotFile)
+		}()
 
-	if listCmdOut, err := cmd.CombinedOutput(); err != nil {
-		fmt.Printf("Error running '%v'\n", cmd.String())
-		fmt.Printf("Debug: %s\n", string(listCmdOut))
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		if listCmdOut, err := cmd.CombinedOutput(); err != nil {
+			fmt.Printf("Error running '%v'\n", cmd.String())
+			fmt.Printf("Debug: %s\n", string(listCmdOut))
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Image written to %v\n", imageFilename)
 	}
 
-	fmt.Println(imageFilename)
 }
