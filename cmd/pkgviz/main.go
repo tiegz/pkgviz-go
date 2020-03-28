7aef1ed556775559526c76d55db8912e3a8d4532
Tieg Zaharia <tieg@tidelift.com>
2020-03-28 17:14:16 -0400

    Call graphviz directly from cmd.

--- cmd/pkgviz/main.go
+++ cmd/pkgviz/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"os/exec"
 
 	"github.com/tiegz/pkgviz/pkg/pkgviz"
 )
@@ -17,6 +20,22 @@ func main() {
 		log.Fatalln("error: no package name given")
 		return
 	}
-	out := pkgviz.WriteGraph(args[0])
-	fmt.Println(out)
+	dotFile := pkgviz.WriteGraph(args[0])
+
+	imageFilename := "out.png"
+	cmd := exec.Command("dot", "-Tpng", "-o", imageFilename)
+	stdin, _ := cmd.StdinPipe()
+	go func() {
+		defer stdin.Close()
+		io.WriteString(stdin, dotFile)
+	}()
+
+	if listCmdOut, err := cmd.CombinedOutput(); err != nil {
+		fmt.Printf("Error running '%v'\n", cmd.String())
+		fmt.Printf("Debug: %s\n", string(listCmdOut))
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println(imageFilename)
 }
